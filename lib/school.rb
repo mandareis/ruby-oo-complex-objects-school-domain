3d8b29054f987434e00812572225e60d0e88fc67
mandareis <amanda@depaula.xyz>
2020-10-07 08:45:31 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+
+    def initialize(name)
+    @name = name
+    @roster ={}
+    end
+    def name
+        @name
+    end
+    def roster
+        @roster
+    end
+    def add_student(name, grade)
+       if @roster[grade] 
+        @roster[grade] << name
+       else 
+        @roster[grade] = []
+        @roster[grade] << name
+       end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    # def sort
+    #  @roster.each do {|key,value| }
+    # end
+end
+
+# school = School.new("Bayside High School")
+# school.add_student("Zach Morris", 9) 
+# school.add_student("Amanda Reis", 8)
+# school.add_student("Nick C.", 9)
+
+# pp school
